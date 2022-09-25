6f7da51c584f5f78da38a08b0e42f8fe2cceac3f
Ashish Singh <ashishsingh.1899@gmail.com>
2022-09-25 22:36:32 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SaveIcon from "@material-ui/icons/Save";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  ButtonGroup,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Container,
+  Paper,
+  Grid,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@material-ui/core";
+
+import {
+  makeStyles,
+  ThemeProvider,
+  createTheme,
+} from "@material-ui/core/styles";
+
+const theme = createTheme({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 12,
+    },
+  },
+});
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #333, #000)",
+    border: 0,
+    marginBottom: 15,
+    borderRadius: 15,
+    color: "white",
+    padding: "0 30px",
+  },
+});
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button className={classes.root}>Test Styled Button</Button>;
+}
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true);
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          icon={<SaveIcon />}
+          checkedIcon={<DeleteIcon />}
+          onChange={(e) => setChecked(e.target.checked)}
+          inputProps={{
+            "aria-label": "secondary checkbox",
+          }}
+        />
+      }
+      label="Testing Checkbox"
+    />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="100%">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary">
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">MUI Theming</Typography>
+                <Button>Login</Button>
+              </Toolbar>
+            </AppBar>
+            <Typography variant="h2" component="div">
+              Welcome to MUI
+            </Typography>
+            <Typography variant="subtitle1">Learn how to use MUI</Typography>
+            <ButtonStyled />
+            <Grid container spacing={2} justifyContent="center">
+              <Grid item xs={3} sm={3} lg={4}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3} sm={3} lg={4}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+              <Grid item xs={3} sm={3} lg={4}>
+                <Paper style={{ height: 75, width: "100%" }} />
+              </Grid>
+            </Grid>
+            <CheckboxExample />
+            <ButtonGroup variant="contained" color="primary">
+              <Button startIcon={<SaveIcon />}>Save</Button>
+              <Button startIcon={<DeleteIcon />}>Discard</Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
